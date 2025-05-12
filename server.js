b62c287f10416712700bb239c97a572a3df6fd6a
Karim Ahmed Wahba <93930431+kkarimwahba@users.noreply.github.com>
2025-05-12 14:00:51 +0300

    Implement code changes to enhance functionality and improve performance

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connectDB = require("./connection");
 // App and Port
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Connect to MongoDB
 connectDB();

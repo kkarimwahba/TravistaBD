3baacfe9fcdc98d6f32b58159500a001e7f63e78
Karim Ahmed Wahba <93930431+kkarimwahba@users.noreply.github.com>
2025-05-14 16:08:26 +0300

    Add sameSite attribute to session cookie for cross-site request handling

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production", // true for HTTPS in production
+      sameSite: "none",
       maxAge: 1000 * 60 * 60 * 24, // Cookie expiration time (1 day)
     },
   })

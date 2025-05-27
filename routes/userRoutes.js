aae735a3526e11557eeeb4b49ce9fa6c460d01f0
khaledsho3er <karimwahba53@gmail.com>
2025-05-27 18:51:38 +0300

    removing authMiddlewaare until we got a domainName

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,14 +13,14 @@ const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.get("/", getUsers); // Get all users
-router.get("/:id", authMiddleware, getUserById); // Get user by ID
-router.put("/:id", authMiddleware, updateUser); // Update user
-router.delete("/:id", authMiddleware, deleteUser); // Delete user
+router.get("/:id", getUserById); // Get user by ID
+router.put("/:id", updateUser); // Update user
+router.delete("/:id", deleteUser); // Delete user
 router.post("/session", loginWithSession); // Login with session
-router.post("/logout", authMiddleware, logoutUser); // Logout
+router.post("/logout", logoutUser); // Logout
 router.post("/login", loginUser); // Login
 // Get current logged-in user
-router.get("/me", authMiddleware, async (req, res) => {
+router.get("/me", async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select(
       "-password, -sessionToken"

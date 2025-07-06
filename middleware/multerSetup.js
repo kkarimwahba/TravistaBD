25ad39a9b9218a2562320459a522dd1c9e4ba645
khaledsho3er <karimwahba53@gmail.com>
2025-07-06 13:00:38 +0300

    Increase file size limit to 20MB in multer upload middleware

--- middleware/multerSetup.js
+++ middleware/multerSetup.js
@@ -32,7 +32,7 @@ const fileFilter = (req, file, cb) => {
 // Upload middleware
 const upload = multer({
   storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB file limit
+  limits: { fileSize: 20 * 1024 * 1024 }, // 20MB file limit
   fileFilter,
 });
 

1bca3e44b0540801d7c627a00454b92792732428
khaledsho3er <karimwahba53@gmail.com>
2025-07-06 17:04:23 +0300

    Set default value of isActive to false in banner schema

--- models/banner.js
+++ models/banner.js
@@ -6,7 +6,7 @@ const bannerSchema = new mongoose.Schema(
     destinations: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
-    isActive: { type: Boolean, default: true },
+    isActive: { type: Boolean, default: false },
     date: { type: Date, default: Date.now },
   },
   { timestamps: true }
